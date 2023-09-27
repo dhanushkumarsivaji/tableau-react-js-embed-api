7714e6c7c5337fa5bc64d5c5a9b2b3e73fc8e29f
Dhanush kumar Sivaji <36889650+dhanushkumarsivaji@users.noreply.github.com>
2023-09-27 11:02:14 +0000

    Update tableau-js-api-3.js

--- src/components/tableau-js-api-3.js
+++ src/components/tableau-js-api-3.js
@@ -1,33 +1,43 @@
-import React from "react";
+import React, { useState } from "react";
 import { useExternalScript } from "../hooks/useExternalScript";
 
 const TableauJSApi = () => {
-  const [toolbarPosition, setToolbarPosition] = React.useState("top");
-  const [viewSrc, setViewSrc] = React.useState(true);
+  const [toolbarPosition] = React.useState("bottom");
+  const viewsSources = [
+    "https://public.tableau.com/views/DhanushsHelloWorld/Dashboard1?:language=en-US&publish=yes&:display_count=n&:origin=viz_share_link",
+    "https://public.tableau.com/views/KPICardwithDimensions/KPICard?:language=en-US&:display_count=n&:origin=viz_share_link",
+    "https://public.tableau.com/views/AJAYDASHBOARD_16950052959310/Dashboard2?:language=en-US&:display_count=n&:origin=viz_share_link",
+  ];
+  const [array] = useState(viewsSources);
+  const [currentIndex, setCurrentIndex] = useState(0);
 
   const tableauScript =
     "https://public.tableau.com/javascripts/api/tableau.embedding.3.latest.min.js";
   const status = useExternalScript(tableauScript, "module");
 
-  const handleChangeToolbarPosition = () => {
-    if (toolbarPosition === "top") {
-      setToolbarPosition("bottom");
-    } else {
-      setToolbarPosition("top");
-    }
-  };
-
   const handleChangeViews = () => {
-    setViewSrc(!viewSrc);
+    setCurrentIndex((prevIndex) => {
+      // Calculate the index of the previous value in a circular manner
+      const newIndex = (prevIndex - 1 + array.length) % array.length;
+      return newIndex;
+    });
   };
 
   const renderButtons = () => (
-    <React.Fragment>
+    <div style={{ margin: "10px", display: "flex", justifyContent: "center" }}>
       <button
-        onClick={() => handleChangeToolbarPosition()}
-      >{` Change the position of toolbar ${toolbarPosition} `}</button>
-      <button onClick={() => handleChangeViews()}>{` Change View`}</button>
-    </React.Fragment>
+        style={{
+          padding: "10px",
+          fontWight: "600",
+          backgroundColor: "black",
+          color: "#fff",
+          borderRadius: "4px",
+          border: "1px solid white",
+          cursor: "pointer",
+        }}
+        onClick={() => handleChangeViews()}
+      >{` Change View`}</button>
+    </div>
   );
 
   return status === "ready" ? (
@@ -36,11 +46,7 @@ const TableauJSApi = () => {
       <div style={{ width: "100%" }}>
         <tableau-viz
           id="tableauViz"
-          src={
-            viewSrc
-              ? "https://public.tableau.com/views/DhanushsHelloWorld/Dashboard1?:language=en-US&publish=yes&:display_count=n&:origin=viz_share_link"
-              : "https://public.tableau.com/views/AJAYDASHBOARD_16950052959310/Dashboard2?:language=en-US&:display_count=n&:origin=viz_share_link"
-          }
+          src={array[currentIndex]}
           toolbar={toolbarPosition}
           hide-tabs
         ></tableau-viz>
@@ -52,6 +58,13 @@ const TableauJSApi = () => {
 };
 
 export default TableauJSApi;
+
+{
+  /* <button
+        onClick={() => handleChangeToolbarPosition()}
+      >{` Change the position of toolbar - ${toolbarPosition} `}</button> */
+}
+
 // const [showTooltip, setShowTooltip] = React.useState(true);
 // <viz-filter
 // field="State/Province"
@@ -59,3 +72,11 @@ export default TableauJSApi;
 // ></viz-filter>
 // <custom-parameter name=":tooltip" value={showTooltip ? "yes" : 'no'}></custom-parameter>
 // <custom-parameter name=":dataDetails" value="no"></custom-parameter>
+
+// const handleChangeToolbarPosition = () => {
+//   if (toolbarPosition === "top") {
+//     setToolbarPosition("bottom");
+//   } else {
+//     setToolbarPosition("top");
+//   }
+// };
